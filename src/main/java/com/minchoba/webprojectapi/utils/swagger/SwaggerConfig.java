f89a2dc2e95e20bfd7ef5ed364b06d68ebbaf67d
minuk8932 <minuk8932@naver.com>
2018-04-02 16:55:50 +0900

    add Posts domain  by lombok

--- src/main/java/com/minchoba/webprojectapi/utils/swagger/SwaggerConfig.java
+++ src/main/java/com/minchoba/webprojectapi/utils/swagger/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.minchoba.webprojectapi.utils.swagger;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableSwagger2
 public class SwaggerConfig {
 	
 }
--- src/test/java/com/minchoba/webprojectapi/WebProjectApiApplicationTests.java
+++ src/test/java/com/minchoba/webprojectapi/WebProjectApiApplicationTests.java
@@ -1,14 +1,26 @@
 package com.minchoba.webprojectapi;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.minchoba.webprojectapi.domain.posts.PostsRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebProjectApiApplicationTests {
-
+	
+	@Autowired
+	PostsRepository postsRepository;
+	
+	@After
+	public void cleanup() {
+		postsRepository.deleteAll();
+	}
+	
 	@Test
 	public void contextLoads() {
 	}
